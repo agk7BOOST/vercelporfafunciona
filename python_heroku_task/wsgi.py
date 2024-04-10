438a3b9c3f746e43ee5c533630ab4fea33dee166
agk7BOOST <147288332+agk7BOOST@users.noreply.github.com>
2024-04-10 15:40:20 -0400

    Update wsgi.py

--- python_heroku_task/wsgi.py
+++ python_heroku_task/wsgi.py
@@ -13,4 +13,4 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'python_heroku_task.settings')
 
-app = get_wsgi_application()
+app = get_wsgi_application() #modificado 
